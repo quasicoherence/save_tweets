9cb0baeb03eb791af8c4bf834af9faf4c0c62c74
Kevin Lin <kevinlin@gmail.com>
2019-09-22 00:08:10 -0400

    add comments

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-
+// If your twitter account is suspended, you won't be able to access your tweets
+// using any standard APIs, nor will you be able to download your archive.
+// However, you will be able to still login on desktop (though not on mobile).
+// After logging in and going to your timeline, you can copy/paste the below
+// code into your javascript console and it will load tweets and put them into
+// the allArticles variable. After the script finishes, it will put all the
+// tweets into the allPosts string which you can then copy/paste and save
+// elsewhere.
+//
+// USE THIS AT YOUR OWN RISK!!! I make no guarantees that anything works.
 
 var allArticles = new Set();
 var allPosts;
@@ -33,7 +42,7 @@ function myLoop() {
       allPosts = Array.from(allArticles).join("\n\n\n");
       alert("All done! Your tweets are in the allPosts variable.")
     }
-  }, 2000); // 2 second wait
+  }, 3000); // 3 second wait
 }
 
 myLoop();
