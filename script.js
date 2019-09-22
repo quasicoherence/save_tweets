7932f2027136c2d6671bdc3480d0215387647eed
Kevin Lin <kevinlin@gmail.com>
2019-09-22 00:28:27 -0400

    new comments

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // If your twitter account is suspended, you won't be able to access your tweets
 // using any standard APIs, nor will you be able to download your archive.
 // However, you will be able to still login on desktop (though not on mobile).
-// After logging in and going to your timeline, you can copy/paste the below
+// After logging in and going to your profile, you can copy/paste the below
 // code into your javascript console and it will load tweets and put them into
 // the allArticles variable. After the script finishes, it will put all the
 // tweets into the allPosts string which you can then copy/paste and save
 // elsewhere.
 //
 // USE THIS AT YOUR OWN RISK!!! I make no guarantees that anything works.
+//
+// Note: This trick can be used for more general scraping purposes as well.
 
 var allArticles = new Set();
 var allPosts;
@@ -24,11 +26,14 @@ function addToArticles(a) {
 }
 
 function getArticlesAndScroll() {
+  // Tweets have HTML tag name "article"
   currArticleElements = document.getElementsByTagName("article");
   currArticles = Array.from(currArticleElements).map(function(article) {
+    // You can alternatively return article.innerHTML if you want the full HTML.
     return article.innerText;
   });
   added = addToArticles(currArticles);
+  // Scroll down by 2000 pixels.
   window.scrollBy(0, 2000);
   return added;
 }
@@ -36,6 +41,9 @@ function getArticlesAndScroll() {
 function myLoop() {
   setTimeout(function() {
     added = getArticlesAndScroll();
+    // This if clause can be changed to whatever you want. As it is, the loop
+    // continues if you've successfully added new tweets to allArticles in the
+    // previous iteration.
     if (added) {
       myLoop();
     } else {
